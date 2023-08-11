25074f2cd6ceaa714e5d85874aeb423f62512fb9
ubeb24 <saludadesj@gmail.com>
2023-08-11 19:58:42 -0400

    initial attempt, still buggy

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// X const { formSubmission } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+window.addEventListener("load", function() {
+   //Validate the user responses with `preventDefault()` to ensure the following: 
+  // a. The user has entered something for every field.
+   //b. The user has entered string values for names and number for fuel and cargo levels.
+   //preventDefault is to stop "default functionality of browser" when we do not meet conditions.
+   let form = document.querySelector("form")
+   let list = document.getElementById("faultyItems")
+   //do i add another function here or do i add the submission form function in script helper when prevent defaulting the reloaded page
+   //formSubmission(event) {event.preventDefault()}
+   //form.addEventListener("submit",formSubmission)
+   //OR
+   //function noReload(event){event.preventDefault()}
+   //form.addEventListener("submit",formSubmission)
+   //OR
+   //do you event listen for "load" and if it does it without the stuff, it can't submit the stuff?
+   form.addEventListener("submit", function(event){
+    event.preventDefault();
+    //call form submission to pass these variables in 
+  
+    let pilotNameInput = document.querySelector("input[name=pilotName]");
+    let copilotNameInput = document.querySelector("input[name=copilotName]");
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+        //  if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput === "" || cargoMassInput === "") {
+        //     alert("All fields are required!");
+        //     // stop the form submission
+        //  } else if ((!String(pilotNameInput))||(!String(copilotNameInput))||(!Number(fuelLevelInput))||(!Number(cargoMassInput))) {
+        //      alert("Please submit a proper input!");
+        //     } 
+        formSubmission(document, list, pilotNameInput.value, copilotNameInput.value, fuelLevelInput.value, cargoMassInput.value)
+//console.log(cargoMassInput.value)
+// formSubmission(document, list, pilotNameInput.value, copilotNameInput.value, fuelLevelInput.value, cargoMassInput.value);
+});
+   function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    console.log(pilot)
+}
+// function validateInput(testInput) {
+//     let testInput = Number(testInput)
+//     if (testInput === Number) {
+//         return("Is a Number")
+//     } else if (isNaN(testInput)) {
+//         return("Not a Number")
+//     } else if (testInput === '') {
+//         return("Empty")
+//     }
+// }
+
+let listedPlanets;
+// Set listedPlanetsResponse equal to the value returned by calling myFetch()
+let listedPlanetsResponse;
+listedPlanetsResponse.then(function (result) {
+   listedPlanets = result;
+    console.log(listedPlanets);
+}).then(function () {
+   console.log(listedPlanets);
+    // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+})
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -15,14 +15,69 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 <img src="">
    */
 }
-
+// STEP #1: Valid information for the fields means that the user submits a value that is easily converted to the correct data type for our 
+//fellow engineers. The pilot and co-pilot names should be strings and the fuel level and cargo mass should be numbers. 
+//To do this, complete the helper function in your scriptHelper.js called validateInput(). 
+//validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate. 
+//In scriptHelper.js, you will use validateInput() to complete the formSubmission() function. 
 function validateInput(testInput) {
-   
+    let testInput = Number(testInput)
+    if (testInput === Number) {
+        return("Is a Number")
+    } else if (isNaN(testInput)) {
+        return("Not a Number")
+    } else if (testInput === '') {
+        return("Empty")
+    }
 }
+// formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass.
+// Using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below. 
+//Make sure to call your formSubmission() function at the appropriate time in your script.js file!
 
+//document parameter is the form. we will be putting in the form in which was coded in the index where we ask the user to input info
+//the values will come from the form submission.
+// I am confused on the role of this function, is it just supposed to pass the values in? Or somehow turn on the faulty items list?
+
+//FORM SUBMISSION DRAFTS
+        //  if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput === "" || cargoMassInput === "") {
+        //     alert("All fields are required!");   
+        //     ((!String(pilotNameInput))||(!String(copilotNameInput))(!Number(fuelLevelInput))||(!Number(cargoMassInput))) {
+        //         alert("Please submit a proper input!");
+
+// The point of using form submission function is to validate options and changing faulty items and the css code so that it relfects to the user they made mistakes in their inputs
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    console.log(pilot)
 }
+    // let statusCheck = document.getElementById("launchStatus")
+        // if PILOT name is empty or it's number THEN {do this}. Otherwise it's correct and {you do this}
+    // if (validateInput(pilotNameInput) === "Is a Number" || validateInput(pilotNameInput) === "Empty") {
+    //     alert("Please submit a proper input! All fields are required!");
+    // } else { = document.querySelector("data-testid=pilotStatus")
+
+    // }
+    //   // if copilot name is empty or it's number THEN {do this}. Otherwise it's correct and {you do this}
+    // if (validateInput(copilotNameInput) === "Is a Number" || validateInput(copilotNameInput) === "Empty") {
+    //     alert("Please submit a proper input! All fields are required!");
+    // } else {
+
+    // }
+    //   // if fuelLevel is empty or it's not a number THEN {do this}. If not, but it is less than 10000L, THEN {do this}. Otherwise it's correct and {you do this}
+    // if (validateInput(fuelLevelInput)=== "Empty" || validateInput(fuelLevelInput) === "Not a Number") {
+    //     alert("Please submit a proper input! All fields are required!");
+    // } else if (fuelLevelInput<10,000) {
+        
+    // } else {
+
+    // } 
+    //   // if cargomass is empty or it's not a number THEN {do this}. If not, but it is more than 10000L, THEN {do this}. Otherwise it's correct and {you do this}
+    // if (validateInput(cargoMassInput)=== "Empty" || validateInput(cargoMassInput) === "Not a Number") {
+    //     alert("Please submit a proper input! All fields are required!");
+    // } else if (cargoMassInput>10,000) {
+        
+    // } else {
+
+//}
+
 
 async function myFetch() {
     let planetsReturned;
